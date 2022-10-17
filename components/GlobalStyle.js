b1d3ca1de8e01a11cffbd3107b2fe1666e2d50f9
Sebastian Scherbes <github@scherbes.de>
2022-10-17 14:52:35 +0200

    add Rubik fonts

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -1,20 +1,30 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-      :root {
-          --text-primary: #0c2c4d;
-          --background-primary: #edf0f5;
-      }
+    :root {
+        --text-primary: #0c2c4d;
+        --background-primary: #edf0f5;
+    }
   
-      * {
-          box-sizing: border-box;
-      }
-  
-      body {
-          font-family: 'Noto Sans', sans-serif;
-          background-color: var(--background-primary);
-          color: var(--text-primary);
-      }
+    /* rubik-regular - latin */
+    @font-face {
+        font-family: 'Rubik';
+        font-style: normal;
+        font-weight: 400;
+        src: local(''),
+            url('/fonts/rubik-v21-latin-regular.woff2') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
+            url('/fonts/rubik-v21-latin-regular.woff') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Rubik';
+        background-color: var(--background-primary);
+        color: var(--text-primary);
+    }
   `;
 
 export default GlobalStyle;
