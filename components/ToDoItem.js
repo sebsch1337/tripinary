1366ff3c3a94e21bf3d863e63fb2778d6ec4c5ab
Sebastian Scherbes <github@scherbes.de>
2022-10-27 16:52:37 +0200

    remove checkbox tag

--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -2,36 +2,50 @@ import styled from "styled-components";
 import unCheckedIcon from "../assets/unchecked.svg";
 import checkedIcon from "../assets/checked.svg";
 import Image from "next/image";
+import deleteSvg from "../assets/delete.svg";
 
 export default function ToDoItem({ toDo }) {
   return (
     <ToDoListItem>
-      <CheckBox id={toDo.id} />
       <Image
         src={toDo.checked ? checkedIcon : unCheckedIcon}
-        width="18px"
-        height="18px"
+        width={toDo.checked ? "17px" : "14px"}
+        height={toDo.checked ? "17px" : "14px"}
         alt={toDo.checked ? "checked" : "unchecked"}
       />
+
       <Description htmlFor={toDo.id} checked={toDo.checked}>
         {toDo.description}
       </Description>
+      <DeleteToDo>
+        <Image src={deleteSvg} width="10px" height="10px" alt="Delete ToDo" />
+      </DeleteToDo>
     </ToDoListItem>
   );
 }
+
+const DeleteToDo = styled.button`
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  justify-self: flex-end;
+`;
 
 const Description = styled.label`
   margin-left: 0.4em;
   text-decoration: ${({ checked }) => (checked ? "line-through" : "none")};
   color: ${({ checked }) => (checked ? "gray" : "var(--primary-text)")};
+
+  &::before {
+    left: 0;
+    right: 0;
+    background-image: url({unCheckedIcon});
+  }
 `;
 
 const ToDoListItem = styled.li`
   padding: 0.5em 0;
   display: flex;
   align-items: center;
-`;
-
-const CheckBox = styled.input.attrs({ type: "checkbox" })`
-  display: none;
 `;
