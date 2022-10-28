32b31c9c11ea60ac1a3c6f96a351f6c5b9fb8d4b
Sebastian Scherbes <github@scherbes.de>
2022-10-28 15:39:06 +0200

    create DeleteModal component

---
+++ components/DeleteModal.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import Image from "next/image";
+import deleteButtonModalSvg from "../assets/deleteButtonModal.svg";
+
+export default function DeleteModal({ name, onDeleteTrip }) {
+  return (
+    <DeleteSection>
+      <DeleteText>{`Are you sure you want to delete ${name}?`}</DeleteText>
+      <StyledButton onClick={onDeleteTrip} aria-label={`Delete ${name}`}>
+        <Image src={deleteButtonModalSvg} width="290px" height="40px" alt="Delete icon" />
+      </StyledButton>
+    </DeleteSection>
+  );
+}
+
+const DeleteSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2em;
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  border: none;
+  background-color: transparent;
+`;
+
+const DeleteText = styled.p``;
