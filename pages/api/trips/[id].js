e4fb7ba79f8dcce97a1d3d0f66400ea580acf775
Sebastian Scherbes <github@scherbes.de>
2022-11-04 13:29:50 +0100

    create validation for trip/id api

--- pages/api/trips/[id].js
+++ pages/api/trips/[id].js
@@ -9,22 +9,16 @@ export default async function handler(req, res) {
 
   await dbConnect();
 
+  if (id.length !== 24 || !id.match("^[a-z0-9]*$")) return res.status(400).json({ error: "Wrong id" });
+
   switch (method) {
     case "GET":
       try {
         const trip = await Trip.findById(id);
+        if (!trip) return res.status(404).json({ error: id + " not found" });
         res.status(200).json(trip);
       } catch (err) {
-        res.status(404).json({ error: "Not found" });
-      }
-      break;
-
-    case "PATCH":
-      try {
-        const updateTrip = await Trip.findByIdAndUpdate(id, { country: req.body });
-        res.status(200).json(updateTrip);
-      } catch (err) {
-        res.status(404).json({ error: "Not found" });
+        res.status(4045).json({ error: id + " not found" });
       }
       break;
 
@@ -32,8 +26,7 @@ export default async function handler(req, res) {
       try {
         const deleteTrip = await Trip.deleteOne({ _id: id });
         if (deleteTrip.deletedCount === 0) return res.status(404).json({ error: "Not found" });
-
-        res.status(200).json(deleteTrip.deletedCount);
+        res.status(200).json();
       } catch (err) {
         res.status(400).json({ error: err });
       }
