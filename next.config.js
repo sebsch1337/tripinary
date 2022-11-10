d1fee21d64f83993a56aabe280273924a2518710
Sebastian Scherbes <github@scherbes.de>
2022-11-10 17:58:51 +0100

    add google and github image sources to next.config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
 };
 
 module.exports = nextConfig;
