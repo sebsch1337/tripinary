d52369a9e0e34da361b3da49dfbd383b79b6b336
Sebastian Scherbes <github@scherbes.de>
2022-11-12 18:49:52 +0100

    add favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import GlobalStyle from "../components/GlobalStyle";
 import { SessionProvider } from "next-auth/react";
+import GlobalHead from "../components/GlobalHead";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <GlobalStyle />
+      <GlobalHead />
       <Component {...pageProps} />
     </SessionProvider>
   );
