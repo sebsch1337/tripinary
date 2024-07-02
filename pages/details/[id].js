15583ec14524ba7608856940ad0af0c068b201dd
Sebastian Scherbes <github@scherbes.de>
2024-07-03 00:33:58 +0200

    add pixabay call to SSR on all pages

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -17,213 +17,229 @@ import Duration from "../../components/Duration";
 
 import { getToDosByDestinationId } from "../../services/toDoService";
 import { getDestinationById } from "../../services/destinationService";
+import { getRandomPixabayImage } from "../../services/pixabayService";
 
 export async function getServerSideProps(context) {
-  const session = await unstable_getServerSession(context.req, context.res, authOptions);
+	const session = await unstable_getServerSession(context.req, context.res, authOptions);
 
-  if (!session) {
-    return { redirect: { destination: "/", permanent: false } };
-  }
+	if (!session) {
+		return { redirect: { destination: "/", permanent: false } };
+	}
 
-  const id = context.params.id;
+	const id = context.params.id;
 
-  const destinationDB = await getDestinationById(id, session.user.email);
-  const toDosDB = await getToDosByDestinationId(id, session.user.email);
+	const [destinationDB, toDosDB] = await Promise.all([
+		await getDestinationById(id, session.user.email),
+		await getToDosByDestinationId(id, session.user.email),
+	]);
+	const destinationImage = await getRandomPixabayImage(destinationDB.name);
 
-  return {
-    props: {
-      id: id,
-      destinationDB,
-      toDosDB,
-    },
-  };
+	const formattedStartDate = new Date(destinationDB.startDate * 1000).toLocaleDateString();
+	const formattedEndDate = new Date(destinationDB.endDate * 1000).toLocaleDateString();
+
+	return {
+		props: {
+			id: id,
+			destinationDB,
+			toDosDB,
+			destinationImage,
+			formattedStartDate,
+			formattedEndDate,
+		},
+	};
 }
 
-export default function Details({ id, destinationDB, toDosDB }) {
-  const [destination, setDestination] = useState(destinationDB);
-  const [toDos, setToDos] = useState(toDosDB);
-  const [modal, setModal] = useState({ visible: false, name: "" });
-  const [loader, setLoader] = useState({ triggered: false, show: false });
+export default function Details({ id, destinationDB, toDosDB, destinationImage, formattedStartDate, formattedEndDate }) {
+	const [destination, setDestination] = useState(destinationDB);
+	const [toDos, setToDos] = useState(toDosDB);
+	const [modal, setModal] = useState({ visible: false, name: "" });
+	const [loader, setLoader] = useState({ triggered: false, show: false });
 
-  const destinationName = destinationDB ? destinationDB.name : "Not found";
+	const destinationName = destinationDB ? destinationDB.name : "Not found";
 
-  const calculateDuration = () => (destination?.endDate - destination?.startDate) / 86400;
+	const calculateDuration = () => (destination?.endDate - destination?.startDate) / 86400;
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (loader.triggered) {
-        setLoader((loader) => ({ triggered: false, show: !loader.show }));
-      }
-    }, 200);
-    return () => clearTimeout(timer);
-  }, [loader.triggered]);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (loader.triggered) {
+				setLoader((loader) => ({ triggered: false, show: !loader.show }));
+			}
+		}, 200);
+		return () => clearTimeout(timer);
+	}, [loader.triggered]);
 
-  const toggleLoader = () => setLoader((loader) => ({ triggered: !loader.triggered, show: loader.show }));
+	const toggleLoader = () => setLoader((loader) => ({ triggered: !loader.triggered, show: loader.show }));
 
-  const toggleModal = (modalName = "", type = "") =>
-    setModal((modal) => ({ visible: !modal.visible, name: modalName, type: type }));
+	const toggleModal = (modalName = "", type = "") =>
+		setModal((modal) => ({ visible: !modal.visible, name: modalName, type: type }));
 
-  const onToggleToDoItem = async (id, checked) => {
-    toggleLoader();
-    const res = await fetch("/api/todos/" + id + "?destinationId=" + destination.id, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ checked: !checked }),
-    });
-    const newToDos = await res.json();
-    setToDos(newToDos);
-    toggleLoader();
-  };
+	const onToggleToDoItem = async (id, checked) => {
+		toggleLoader();
+		const res = await fetch("/api/todos/" + id + "?destinationId=" + destination.id, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ checked: !checked }),
+		});
+		const newToDos = await res.json();
+		setToDos(newToDos);
+		toggleLoader();
+	};
 
-  const onUpdateDetail = async (updated) => {
-    toggleLoader();
-    const res = await fetch("/api/destinations/" + id, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ...destination, ...updated }),
-    });
-    const newDestination = await res.json();
-    setDestination(newDestination);
-    toggleLoader();
-  };
+	const onUpdateDetail = async (updated) => {
+		toggleLoader();
+		const res = await fetch("/api/destinations/" + id, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ ...destination, ...updated }),
+		});
+		const newDestination = await res.json();
+		setDestination(newDestination);
+		toggleLoader();
+	};
 
-  const onSubmitNewToDoItem = async (toDo) => {
-    toggleLoader();
-    const res = await fetch("/api/todos/?destinationId=" + id, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ description: toDo }),
-    });
-    const newToDos = await res.json();
-    setToDos(newToDos);
-    toggleLoader();
+	const onSubmitNewToDoItem = async (toDo) => {
+		toggleLoader();
+		const res = await fetch("/api/todos/?destinationId=" + id, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ description: toDo }),
+		});
+		const newToDos = await res.json();
+		setToDos(newToDos);
+		toggleLoader();
 
-    setTimeout(() => {
-      window.scrollBy({ top: 100, behavior: "smooth" });
-    }, 100);
-  };
+		setTimeout(() => {
+			window.scrollBy({ top: 100, behavior: "smooth" });
+		}, 100);
+	};
 
-  const onDeleteToDoItem = async (id, destinationId) => {
-    toggleLoader();
-    const res = await fetch("/api/todos/" + id + "?destinationId=" + destinationId, {
-      method: "DELETE",
-    });
-    const newToDos = await res.json();
-    setToDos(newToDos);
-    toggleLoader();
-  };
+	const onDeleteToDoItem = async (id, destinationId) => {
+		toggleLoader();
+		const res = await fetch("/api/todos/" + id + "?destinationId=" + destinationId, {
+			method: "DELETE",
+		});
+		const newToDos = await res.json();
+		setToDos(newToDos);
+		toggleLoader();
+	};
 
-  return (
-    <>
-      <Header coverImage={destinationName} />
-      <Main>
-        <DetailHeadline>{destinationName.toUpperCase()}</DetailHeadline>
-        {destination && (
-          <DetailSection>
-            <DetailTitle>
-              Dates
-              <EditButton toggleModal={() => toggleModal("Dates")} />
-            </DetailTitle>
-            <DetailText>
-              {new Date(destination.startDate * 1000).toLocaleDateString() +
-                ` until ` +
-                new Date(destination.endDate * 1000).toLocaleDateString()}
-            </DetailText>
-            <DetailTitle>
-              Hotel
-              <EditButton toggleModal={() => toggleModal("Hotel")} />
-            </DetailTitle>
-            <DetailText>{destination.hotel === "" ? "<No hotel booked yet>" : destination.hotel}</DetailText>
-            <DetailTitle>
-              Transport
-              <EditButton toggleModal={() => toggleModal("Transport")} />
-            </DetailTitle>
-            <DetailText>
-              {destination.transport === "" ? "<No transportation booked yet>" : destination.transport}
-            </DetailText>
-            <DetailTitle>To-Do</DetailTitle>
-            <ToDoWrapper>
-              {toDos &&
-                toDos.map((toDo) => (
-                  <ToDoItem
-                    key={toDo.id}
-                    toDo={toDo}
-                    onDeleteToDoItem={() => onDeleteToDoItem(toDo.id, toDo.destinationId)}
-                    onToggleToDoItem={() => onToggleToDoItem(toDo.id, toDo.checked)}
-                  />
-                ))}
-            </ToDoWrapper>
-            <ToDoForm onSubmitNewToDoItem={onSubmitNewToDoItem} />
-          </DetailSection>
-        )}
-      </Main>
-      <Footer>
-        <Duration title="Duration" number={calculateDuration()} type="night" />
-      </Footer>
-      {modal.visible && (
-        <Modal name={modal.name} toggleModal={toggleModal}>
-          {modal.name === "Dates" && (
-            <EditDatesForm
-              destination={destination}
-              onUpdateDetail={(startDate, endDate) => {
-                toggleModal();
-                onUpdateDetail({ startDate, endDate });
-              }}
-            />
-          )}
-          {modal.name === "Hotel" && (
-            <EditTextForm
-              placeholder="Accommodation"
-              prevTextValue={destination.hotel}
-              onUpdateDetail={(hotel) => {
-                toggleModal();
-                onUpdateDetail({ hotel });
-              }}
-            />
-          )}
-          {modal.name === "Transport" && (
-            <EditTextForm
-              placeholder="Flight / Bus / Train"
-              prevTextValue={destination.transport}
-              onUpdateDetail={(transport) => {
-                toggleModal();
-                onUpdateDetail({ transport });
-              }}
-            />
-          )}
-        </Modal>
-      )}
-      {loader.show && <Loader />}
-    </>
-  );
+	return (
+		<>
+			<Header coverImage={destinationImage} />
+			<Main>
+				<DetailHeadline>{destinationName.toUpperCase()}</DetailHeadline>
+				{destination && (
+					<DetailSection>
+						<DetailTitle>
+							Dates
+							<EditButton toggleModal={() => toggleModal("Dates")} />
+						</DetailTitle>
+						<DetailText>
+							{/* {new Date(destination.startDate * 1000).toLocaleDateString() +
+								` until ` +
+								new Date(destination.endDate * 1000).toLocaleDateString()} */}
+							{`${formattedStartDate} until ${formattedEndDate}`}
+						</DetailText>
+						<DetailTitle>
+							Hotel
+							<EditButton toggleModal={() => toggleModal("Hotel")} />
+						</DetailTitle>
+						<DetailText>{destination.hotel === "" ? "<No hotel booked yet>" : destination.hotel}</DetailText>
+						<DetailTitle>
+							Transport
+							<EditButton toggleModal={() => toggleModal("Transport")} />
+						</DetailTitle>
+						<DetailText>{destination.transport === "" ? "<No transportation booked yet>" : destination.transport}</DetailText>
+						<DetailTitle>To-Do</DetailTitle>
+						<ToDoWrapper>
+							{toDos &&
+								toDos.map((toDo) => (
+									<ToDoItem
+										key={toDo.id}
+										toDo={toDo}
+										onDeleteToDoItem={() => onDeleteToDoItem(toDo.id, toDo.destinationId)}
+										onToggleToDoItem={() => onToggleToDoItem(toDo.id, toDo.checked)}
+									/>
+								))}
+						</ToDoWrapper>
+						<ToDoForm onSubmitNewToDoItem={onSubmitNewToDoItem} />
+					</DetailSection>
+				)}
+			</Main>
+			<Footer>
+				<Duration
+					title="Duration"
+					number={calculateDuration()}
+					type="night"
+				/>
+			</Footer>
+			{modal.visible && (
+				<Modal
+					name={modal.name}
+					toggleModal={toggleModal}
+				>
+					{modal.name === "Dates" && (
+						<EditDatesForm
+							destination={destination}
+							onUpdateDetail={(startDate, endDate) => {
+								toggleModal();
+								onUpdateDetail({ startDate, endDate });
+							}}
+						/>
+					)}
+					{modal.name === "Hotel" && (
+						<EditTextForm
+							placeholder="Accommodation"
+							prevTextValue={destination.hotel}
+							onUpdateDetail={(hotel) => {
+								toggleModal();
+								onUpdateDetail({ hotel });
+							}}
+						/>
+					)}
+					{modal.name === "Transport" && (
+						<EditTextForm
+							placeholder="Flight / Bus / Train"
+							prevTextValue={destination.transport}
+							onUpdateDetail={(transport) => {
+								toggleModal();
+								onUpdateDetail({ transport });
+							}}
+						/>
+					)}
+				</Modal>
+			)}
+			{loader.show && <Loader />}
+		</>
+	);
 }
 
 const ToDoWrapper = styled.ul`
-  list-style: none;
-  all: unset;
+	list-style: none;
+	all: unset;
 `;
 
 const DetailTitle = styled.h3`
-  color: var(--drop-shadow);
-  display: flex;
-  justify-content: space-between;
+	color: var(--drop-shadow);
+	display: flex;
+	justify-content: space-between;
 `;
 
 const DetailText = styled.p`
-  margin-bottom: 1rem;
+	margin-bottom: 1rem;
 `;
 
 const DetailSection = styled.section`
-  margin: 0 2rem 5rem;
+	margin: 0 2rem 5rem;
 `;
 
 const DetailHeadline = styled.h1`
-  margin: 2rem 0 1rem;
-  text-align: center;
+	margin: 2rem 0 1rem;
+	text-align: center;
 `;
--- pages/index.js
+++ pages/index.js
@@ -18,187 +18,224 @@ import UserButton from "../components/Buttons/UserButton";
 import UserProfile from "../components/Modals/UserProfile";
 import Modal from "../components/Modals/Modal";
 import DeleteModal from "../components/Modals/DeleteModal";
+import { getRandomPixabayImage } from "../services/pixabayService";
 
 export async function getServerSideProps(context) {
-  const session = await unstable_getServerSession(context.req, context.res, authOptions);
+	const session = await unstable_getServerSession(context.req, context.res, authOptions);
+	if (!session) return { props: {} };
 
-  if (session) {
-    const tripsDb = await getAllTrips(session.user.email);
-    return { props: { tripsDb: tripsDb } };
-  }
-
-  return { props: {} };
+	const allTrips = await getAllTrips(session.user.email);
+	const tripsDb = await Promise.all(
+		allTrips.map(async (trip) => {
+			try {
+				const image = await getRandomPixabayImage(trip?.country);
+				return { ...trip, image };
+			} catch (error) {
+				return { ...trip, image: { largeImageURL: "/public/not-found.webp" } };
+			}
+		})
+	);
+	return { props: { tripsDb } };
 }
 
 export default function Home({ tripsDb }) {
-  const { data: session } = useSession();
-  const [trips, setTrips] = useState(tripsDb);
-  const [showProfile, setShowProfile] = useState(false);
-  const [modal, setModal] = useState({ visible: false, name: "" });
-  const [loader, setLoader] = useState({ triggered: false, show: false });
+	const { data: session } = useSession();
+	const [trips, setTrips] = useState(tripsDb);
+	const [showProfile, setShowProfile] = useState(false);
+	const [modal, setModal] = useState({ visible: false, name: "" });
+	const [loader, setLoader] = useState({ triggered: false, show: false });
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (loader.triggered) {
-        setLoader((loader) => ({ triggered: false, show: !loader.show }));
-      }
-    }, 200);
-    return () => clearTimeout(timer);
-  }, [loader.triggered]);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (loader.triggered) {
+				setLoader((loader) => ({ triggered: false, show: !loader.show }));
+			}
+		}, 200);
+		return () => clearTimeout(timer);
+	}, [loader.triggered]);
 
-  const toggleLoader = () => setLoader((loader) => ({ triggered: !loader.triggered, show: loader.show }));
+	const toggleLoader = () => setLoader((loader) => ({ triggered: !loader.triggered, show: loader.show }));
 
-  const toggleShowProfile = () => setShowProfile((showProfile) => !showProfile);
+	const toggleShowProfile = () => setShowProfile((showProfile) => !showProfile);
 
-  const toggleModal = (modalName = "", type = "") =>
-    setModal((modal) => ({ visible: !modal.visible, name: modalName, type: type }));
+	const toggleModal = (modalName = "", type = "") =>
+		setModal((modal) => ({ visible: !modal.visible, name: modalName, type: type }));
 
-  const onSubmitNewTrip = async (event) => {
-    event.preventDefault();
-    const countryName = event.target.country.value;
+	const onSubmitNewTrip = async (event) => {
+		event.preventDefault();
+		const countryName = event.target.country.value;
 
-    toggleLoader();
-    const res = await fetch("/api/trips", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ country: countryName, userEmail: session.user.email }),
-    });
-    const newTrips = await res.json();
-    toggleLoader();
-    if (newTrips.message) return alert(newTrips.message);
-    setTrips(newTrips);
+		toggleLoader();
+		const res = await fetch("/api/trips", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ country: countryName, userEmail: session.user.email }),
+		});
+		const newTrips = await res.json();
+		toggleLoader();
+		if (newTrips.message) return alert(newTrips.message);
+		addImagesToTrips(newTrips);
 
-    event.target.reset();
-    setTimeout(() => {
-      window.scrollBy({ top: 300, behavior: "smooth" });
-    }, 100);
-  };
+		event.target.reset();
+		setTimeout(() => {
+			window.scrollBy({ top: 300, behavior: "smooth" });
+		}, 100);
+	};
 
-  const onDeleteAccount = async () => {
-    const res = await fetch("/api/trips", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({}),
-    });
+	const onDeleteAccount = async () => {
+		const res = await fetch("/api/trips", {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({}),
+		});
 
-    const deletedAccount = await res;
-    if (deletedAccount.error) return alert(deletedAccount.error);
-  };
+		const deletedAccount = await res.json();
+		if (deletedAccount.error) return alert(deletedAccount.error);
+	};
 
-  return (
-    <>
-      {session ? (
-        <>
-          <Header>
-            {!showProfile && session && <UserButton img={session.user.image} onClick={toggleShowProfile} />}
-            <TripsHeadline>
-              Start
-              <br />
-              your journey
-            </TripsHeadline>
-          </Header>
+	const addImagesToTrips = async (newTrips) => {
+		const tripsWithImages = await Promise.all(
+			await newTrips.map(async (trip) => {
+				const res = await fetch(`/api/images?q=${trip?.country}`);
+				const image = await res.json();
+				return { ...trip, image };
+			})
+		);
 
-          <Main>
-            {showProfile && (
-              <UserProfile
-                session={session}
-                toggleShowProfile={toggleShowProfile}
-                showProfile={showProfile}
-                deleteAccount={() => toggleModal("account", "account")}
-                signOut={() => {
-                  toggleLoader();
-                  signOut();
-                }}
-              />
-            )}
-            <TripList trips={trips} onSubmitNewTrip={onSubmitNewTrip} loader={loader} />
-          </Main>
-        </>
-      ) : (
-        <LoginMain>
-          <LogoArea>
-            <Image src={logoSvg} width="150" height="150" alt="logo" />
-            <LogoText>TRIPINARY</LogoText>
-          </LogoArea>
+		setTrips(tripsWithImages);
+	};
 
-          <LoginButton
-            icon={gitHubSvg}
-            providerName="GitHub"
-            bgColor="#24292e"
-            onClick={() => {
-              toggleLoader();
-              signIn("github");
-            }}
-          />
-          <LoginButton
-            icon={googleSvg}
-            providerName="Google"
-            bgColor="#DB4437"
-            onClick={() => {
-              toggleLoader();
-              signIn("google");
-            }}
-          />
-        </LoginMain>
-      )}
-      {modal.visible && modal.type === "account" && (
-        <Modal name={`Delete ${modal.name}`} toggleModal={toggleModal}>
-          <DeleteModal
-            name={modal.name}
-            onClick={async () => {
-              toggleLoader();
-              toggleModal();
-              toggleShowProfile();
-              await onDeleteAccount();
-              signOut();
-            }}
-          />
-        </Modal>
-      )}
-      {loader.show && <Loader />}
-    </>
-  );
+	return (
+		<>
+			{session ? (
+				<>
+					<Header>
+						{!showProfile && session && (
+							<UserButton
+								img={session.user.image}
+								onClick={toggleShowProfile}
+							/>
+						)}
+						<TripsHeadline>
+							Start
+							<br />
+							your journey
+						</TripsHeadline>
+					</Header>
+
+					<Main>
+						{showProfile && (
+							<UserProfile
+								session={session}
+								toggleShowProfile={toggleShowProfile}
+								showProfile={showProfile}
+								deleteAccount={() => toggleModal("account", "account")}
+								signOut={() => {
+									toggleLoader();
+									signOut();
+								}}
+							/>
+						)}
+						<TripList
+							trips={trips}
+							onSubmitNewTrip={onSubmitNewTrip}
+							loader={loader}
+						/>
+					</Main>
+				</>
+			) : (
+				<LoginMain>
+					<LogoArea>
+						<Image
+							src={logoSvg}
+							width="150"
+							height="150"
+							alt="logo"
+						/>
+						<LogoText>TRIPINARY</LogoText>
+					</LogoArea>
+
+					<LoginButton
+						icon={gitHubSvg}
+						providerName="GitHub"
+						bgColor="#24292e"
+						onClick={() => {
+							toggleLoader();
+							signIn("github");
+						}}
+					/>
+					<LoginButton
+						icon={googleSvg}
+						providerName="Google"
+						bgColor="#DB4437"
+						onClick={() => {
+							toggleLoader();
+							signIn("google");
+						}}
+					/>
+				</LoginMain>
+			)}
+			{modal.visible && modal.type === "account" && (
+				<Modal
+					name={`Delete ${modal.name}`}
+					toggleModal={toggleModal}
+				>
+					<DeleteModal
+						name={modal.name}
+						onClick={async () => {
+							toggleLoader();
+							toggleModal();
+							toggleShowProfile();
+							await onDeleteAccount();
+							signOut();
+						}}
+					/>
+				</Modal>
+			)}
+			{loader.show && <Loader />}
+		</>
+	);
 }
 
 const Header = styled.header``;
 
 const TripsHeadline = styled.h1`
-  margin: 1.5em 1em 1em 0.8em;
-  font-size: 2.5rem;
-  font-weight: 500;
+	margin: 1.5em 1em 1em 0.8em;
+	font-size: 2.5rem;
+	font-weight: 500;
 `;
 
 const LogoArea = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  flex-wrap: wrap;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	flex-wrap: wrap;
 `;
 
 const LogoText = styled.h1`
-  color: #316bff;
-  text-align: center;
-  font-size: 2.5rem;
+	color: #316bff;
+	text-align: center;
+	font-size: 2.5rem;
 `;
 
 const LoginMain = styled.main`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  flex-wrap: wrap;
-  gap: 1.5em;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	flex-wrap: wrap;
+	gap: 1.5em;
 `;
 
 const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  flex-wrap: nowrap;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	flex-wrap: nowrap;
 `;
